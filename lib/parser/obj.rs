0746bf125c4fd423b8ddb6fc332ea13ad755279e
Andrew Goacher <a.goacher@shieldsafety.co.uk>
2019-01-03 12:49:44 +0000

    Default implementations of methods + initial parse method

--- lib/parser/obj.rs
+++ lib/parser/obj.rs
@@ -25,10 +25,35 @@ pub struct Vertex {
     w: f32,
 }
 
+impl Vertex {
+    fn new(x: f32, y: f32, z: f32) -> Vertex {
+        Vertex {
+            x: x,
+            y: y,
+            z: z,
+            w: 1.0,
+        }
+    }
+
+    fn with_w(self, w: f32) -> Vertex {
+        Vertex { w: w, ..self }
+    }
+}
+
 pub struct VertexPoint {
     u: f32,
     v: f32,
     w: f32,
+}
+
+impl VertexPoint {
+    fn new(u: f32, v: f32) -> VertexPoint {
+        VertexPoint { u: u, v: v, w: 1.0 }
+    }
+
+    fn with_w(self, w: f32) -> VertexPoint {
+        VertexPoint { w: w, ..self }
+    }
 }
 
 pub struct VertexNormal {
@@ -37,10 +62,26 @@ pub struct VertexNormal {
     k: f32,
 }
 
+impl VertexNormal {
+    fn new(i: f32, j: f32, k: f32) -> VertexNormal {
+        VertexNormal { i: i, j: j, k: k }
+    }
+}
+
 pub struct VertexTexture {
     u: f32,
     v: f32,
     w: f32,
+}
+
+impl VertexTexture {
+    fn new(u: f32, v: f32) -> VertexTexture {
+        VertexTexture { u: u, v: v, w: 0.0 }
+    }
+
+    fn with_w(self, w: f32) -> VertexTexture {
+        VertexTexture { w: w, ..self }
+    }
 }
 
 pub struct ObjectFile {
@@ -51,6 +92,28 @@ pub struct ObjectFile {
     vertex_textures: Vec<VertexTexture>,
     faces: Vec<Face>,
     groups: Vec<String>,
+}
+
+impl ObjectFile {
+    fn parse(file: &str) -> ObjectFile {
+        let mut materials: Vec<String> = Vec::new();
+        let mut vertices: Vec<Vertex> = Vec::new();
+        let mut points: Vec<VertexPoint> = Vec::new();
+        let mut normals: Vec<VertexNormal> = Vec::new();
+        let mut textures: Vec<VertexTexture> = Vec::new();
+        let mut faces: Vec<Face> = Vec::new();
+        let mut groups: Vec<String> = Vec::new();
+
+        ObjectFile {
+            materials: materials,
+            vertices: vertices,
+            vertex_points: points,
+            vertex_normals: normals,
+            vertex_textures: textures,
+            faces: faces,
+            groups: groups,
+        }
+    }
 }
 
 pub enum Dissolve {
@@ -85,5 +148,5 @@ pub struct Material {
     // Ke
     emissive_coefficient: Reflectivity,
     map_ka: String,
-    map_kd: String
+    map_kd: String,
 }
