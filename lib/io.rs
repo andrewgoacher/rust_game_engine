3897b5d421033b7694de23fe06c7fb30141f3699
Andrew Goacher <andrewgoacher@outlook.com>
2019-01-13 15:21:25 +0000

    Added comments to the io lib

--- lib/io.rs
+++ lib/io.rs
@@ -1,6 +1,20 @@
+//! A helper module for common io patterns and functions
 use std::fs::File;
 use std::io::{prelude::*, Cursor};
 
+/// This method will return a cursor that has a collection of unsigned bytes.AsMut
+/// 
+/// # Arguments
+/// 
+/// `file` - a file to load
+/// 
+/// # Example
+/// 
+/// ```rust,no_run 
+/// let mut file = File::open("path/to.file").unwrap();
+/// 
+/// let cursor = to_cursor(mut file);
+/// ```
 pub fn to_cursor(mut file: File) -> Cursor<Vec<u8>> {
     let mut contents = vec![];
     file.read_to_end(&mut contents).ok();
