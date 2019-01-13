19d727bbe96ef3ab81c9818a06a9c1585d5d14fa
Andrew Goacher <andrewgoacher@outlook.com>
2019-01-13 15:13:37 +0000

    Comments for lib module and parser module

--- lib/lib.rs
+++ lib/lib.rs
@@ -1,3 +1,40 @@
+//! A small engine framework using glium and opengl
+//! 
+//! This library is mostly a learning environment for personal use but its aim is
+//! to be a performant 2d and 3d engine.
+//! # Organisation
+//! 
+//! This crate contains several modules [`graphics`] contains the majority of the render code, [`math`] contains a set of utilities for Vector and Matrix maths
+//! creating the actual display / engine requires use of [`engine`] and creating a game requires implemting the trait `Game` from within the [`game`] module.
+//! 
+//! A minimal implementation would be
+//! ```
+//! rust_game_engine::game::Game;
+//! 
+//! struct Demo {}
+//! impl Game for Demo {
+//!     fn on_frame(self, frame: &mut glium::Frame) -> Demo {
+//!         Demo {}
+//!     }
+//! }
+//! ```
+//! 
+//! To run the demo you need to create an events loop and an engine
+//! 
+//! ```
+//! let mut events_loop = glium::glutin::EventsLoop::new();
+//! let display = create_engine(&events_loop, "Window Title");
+//! let game = Demo {};
+//! 
+//! ```
+//! 
+//! The [`engine`] module exposes a method `run` to run the game
+//! 
+//! ``` 
+//! use rust_game_engine::engine::run;
+//! 
+//! run(&display, &mut events_loop, game);
+//! ```
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 
@@ -12,7 +49,3 @@ pub mod graphics;
 pub mod io;
 pub mod math;
 pub mod parser;
-
-pub trait Printable {
-    fn print(&self);
-}
--- lib/parser.rs
+++ lib/parser.rs
@@ -1,10 +1,52 @@
+#[derive(Debug)]
+/// A helper enum to represent parsing errors
 pub enum ParseError {
+    /// The token being parsed is unrecognised.
+    /// The string should represent the token
     UnknownToken(String),
+    /// A general error has been encountered that there is no
+    /// specific error for.
     GeneralError(String),
 }
 
+/// A trait that exposes a parse like behaviour
+/// intended 
 pub trait FromFile {
+    /// Represents the result of the parse 
+    /// This should handle both a succesful or unsuccessful parse
     type ParseResult;
 
+    /// a method to handle parsing something from a file provided
+    /// 
+    /// # Arguments
+    /// 
+    /// `file` - the path to the file to parse
+    /// 
+    /// # Remarks
+    /// 
+    /// It's recommended that when implementing this trait, the ParseResult is 
+    /// `Result<T, ParseError>`
+    /// 
+    /// # Example
+    /// 
+    /// ```rust,no_run
+    /// struct ParsedResult {}
+    /// 
+    /// impl FromFile for ParsedResult {
+    ///     type ParseResult = Result<ParsedResult, ParseError>;
+    /// 
+    ///     fn from_file(file: &str) -> Self::ParsedResult {
+    ///         Ok(ParsedResult)
+    ///     }
+    /// }
+    /// 
+    /// match ParsedResult::from_file("Some/path/to.file") {
+    ///     Err(e) => match e {
+    ///         ParseError::UnknownToken(token) => panic!("Token"),
+    ///         ParseError::GeneralError(error) => panic!("Generic Error")
+    ///     },
+    ///     Ok(result) => result
+    /// }
+    /// ```
     fn from_file(file: &str) -> Self::ParseResult;
 }
